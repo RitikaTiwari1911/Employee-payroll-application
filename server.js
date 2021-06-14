0ac842f6f4bfe86512cb31fd73387e6725d0b17e
RitikaTiwari1911 <spk2ritika1911@gmail.com>
2021-06-14 22:27:51 +0530

    feat: configure and connect to the database

--- server.js
+++ server.js
@@ -6,6 +6,21 @@ const app = express();
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 
+//Configuring the database
+const dbConfig = require('./config/database.config.js');
+const mongoose = require('mongoose')
+
+mongoose.Promise = global.Promise;
+
+//Connecting the database
+mongoose.connect(dbConfig.url,{
+    useNewUrlParser: true
+}).then(()=>{
+    console.log("Succesfully connected to the database");
+}).catch(err =>{
+    console.log('Could not connect to the database. Exiting now...',err);
+    process.exit();
+})
 //defining a simple route
 app.get('/',(req,res) => {
     res.json({
